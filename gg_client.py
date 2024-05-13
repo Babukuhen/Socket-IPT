3a286f94175ee46182179ad6eaf1214e6b09db64
Julius Vincent D. Llavan <juliusllavan@gmail.com>
2024-05-13 20:23:19 +0800

    'Username' & 'Guess' Section Updated

--- gg_client.py
+++ gg_client.py
@@ -1,23 +1,36 @@
 import socket
 import time
 
+username_lock = False
+
+
 host = 'localhost'                              
 port = 7777 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host, port))
 time.sleep(0)
 
-data = s.recv(1024)                             # Receive [UI]
-print(data.decode().strip())                    # Print [UI]
-
 while True:
-    user_input = input('').strip()
-    s.sendall(user_input.encode())              # Send [User Input] (Encode)
-    result = s.recv(1024).decode().strip()      # Receive [Result]
-    if "Correct" in result:
-        print(result)
-        break
+    if not username_lock:                   
+        username_str = s.recv(1024)                     # Receive 'Username' String
+        print(username_str.decode())                    # Print 'Username' String
+        username_input = input('U: ').strip()           # Input 'Username'
+        s.sendall(username_input.encode())              # Send 'Username' Input
+        username_lock = True                            # Lock 'Username'
+
+    guess_str = s.recv(1024)                            # Receive ['Guess' String]
+    print(guess_str.decode())                           # Print ['Guess' String]
+    guess_input = input('G: ').strip()                  # Input 'Guess'
+    s.sendall(guess_input.encode())                     # Send ['Guess' Input]
+
+    result_str = s.recv(1024).decode()                  # Receive ['Result' String]
+    if "Correct" in result_str:
+        print(result_str)
+        # 'Play Again' Feature
+        # Yes - New Difficulty, Unlock Difficulty
+        # No - Exit, New User Can Play, Unlock User & Difficulty
+
     else:
-        print(result)
+        print(result_str)
 
 s.close()
--- gg_server.py
+++ gg_server.py
@@ -4,11 +4,12 @@ import random
 
 host = ''       
 port = 7777
-ui = '''== Guessing Game ==
-Enter You Guess: '''
+
 correct_guess = 0
 client_socket = None
 
+username_str = '== Guessing Game ==\nEnter Username: '
+username_lock = False
 
 # Initialize Socket Object
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -22,20 +23,35 @@ while True:
         print('Waiting for Connection...')
         client_socket, client_address = s.accept()
         print(f'Accepted Connection from {client_address[0]}')
-        client_socket.sendall(ui.encode())                      # Send [UI] (Encode)
         correct_guess = random.randint(1, 100)
 
     else:
-        client_input = client_socket.recv(1024)                 # Receive [User Input]
-        guess = int(client_input.decode().strip())              # Decode [User Input]
-        print(f'User Guess Attempt: {guess}')
-        if guess == correct_guess:
-            client_socket.sendall(b'Correct Answer!')           # Send [Result]         # b'' == ''.encode()
-            client_socket.close()
+        if not username_lock:
+            client_socket.sendall(username_str.encode())                    # Send 'Username' String
+            username_input = client_socket.recv(1024)                       # Receive 'Username' Input
+            username_ans = str(username_input.decode().strip())             # Decode 'Username' Input
+            username_lock = True                                            # Lock 'Username'
+            print(f'Username: {username_ans}')
+
+        guess_str = f'\n[{correct_guess}] Enter Your Guess: '
+        client_socket.sendall(guess_str.encode())                           # Send ['Guess' Input]
+        guess_input = client_socket.recv(1024)                              # Receive ['Guess' Input]
+        guess_ans = int(guess_input.decode().strip())                       # Decode ['Guess' Input]
+        print(f'User Guess Attempt: {guess_ans}')
+        
+        if guess_ans == correct_guess:
+            client_socket.sendall(b'Correct Answer!')                       # Send ['Result' String]
+            # 'Play Again' Feature
+            # Yes - New Difficulty, Unlock Difficulty
+            # No - Exit, New User Can Play, Unlock User & Difficulty
+            
+
             print()
+            client_socket.close()
             client_socket = None
-        elif guess > correct_guess:
-            client_socket.sendall(b'Wrong Answer! (Lower)\n\nEnter Your Guess:')        # Send [Result]
-        elif guess < correct_guess:
-            client_socket.sendall(b'Wrong Answer! (Higher)\n\nEnter Your Guess:')       # Send Result]
+
+        elif guess_ans > correct_guess:
+            client_socket.sendall(b'Wrong Answer! (Lower)\n\nEnter Your Guess:')        # Send ['Result' String]
+        elif guess_ans < correct_guess:
+            client_socket.sendall(b'Wrong Answer! (Higher)\n\nEnter Your Guess:')       # Send ['Result' String]
 s.close()
