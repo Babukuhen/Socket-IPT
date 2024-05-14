7d5ee26d85e1ab8446dd2d55bda224369e3f5d97
Julius Vincent D. Llavan <juliusllavan@gmail.com>
2024-05-14 23:38:58 +0800

    Fix Spacing

--- gg_client.py
+++ gg_client.py
@@ -9,7 +9,7 @@ port = 7777
 username_lock = False
 diff_lock = False
 
-def display_file_contents(file_path):                   # Display File Contents [Up to 10 Lines]
+def display_file_contents(file_path):                                   # Display File Contents [Up to 10 Lines]
     print('\n== Leaderboard ==\n')
     with open(file_path, 'r') as file:
         line_count = 0
@@ -28,42 +28,42 @@ time.sleep(0)
 
 while True:
     if not username_lock:                   
-        username_str = s.recv(1024)                     # Receive 'Username' String
-        print(username_str.decode())                    # Print 'Username' String
-        username_input = input('U: ').strip()           # Input 'Username'
-        s.sendall(username_input.encode())              # Send 'Username' Input
-        username_lock = True                            # Lock 'Username'
+        username_str = s.recv(1024)                                     # Receive 'Username' String
+        print(username_str.decode())                                    # Print 'Username' String
+        username_input = input('U: ').strip()                           # Input 'Username'
+        s.sendall(username_input.encode())                              # Send 'Username' Input
+        username_lock = True                                            # Lock 'Username'
 
     if not diff_lock:
-        diff_str = s.recv(1024)                         # Receive ['Difficulty' String]
-        print(diff_str.decode())                        # Print ['Difficulty' String]
-        diff_input = input('D: ').strip().upper()       # Input 'Difficulty'
+        diff_str = s.recv(1024)                                         # Receive ['Difficulty' String]
+        print(diff_str.decode())                                        # Print ['Difficulty' String]
+        diff_input = input('D: ').strip().upper()                       # Input 'Difficulty'
         if diff_input == 'A': file_path = 'leaderboard_easy.txt'
         elif diff_input == 'B': file_path = 'leaderboard_medium.txt'
         elif diff_input == 'C': file_path = 'leaderboard_hard.txt'
-        s.sendall(diff_input.encode())                  # Send ['Difficulty' Input]
-        diff_lock = True                                # Lock 'Difficulty'
+        s.sendall(diff_input.encode())                                  # Send ['Difficulty' Input]
+        diff_lock = True                                                # Lock 'Difficulty'
 
-        display_file_contents(file_path)                # Display Txt File [Line by Line, Up To 10]
+        display_file_contents(file_path)                                # Display Txt File [Line by Line, Up To 10]
 
-    guess_str = s.recv(1024)                            # Receive ['Guess' String]
-    print(guess_str.decode())                           # Print ['Guess' String]
-    guess_input = input('G: ').strip()                  # Input 'Guess'
-    s.sendall(guess_input.encode())                     # Send ['Guess' Input]
+    guess_str = s.recv(1024)                                            # Receive ['Guess' String]
+    print(guess_str.decode())                                           # Print ['Guess' String]
+    guess_input = input('G: ').strip()                                  # Input 'Guess'
+    s.sendall(guess_input.encode())                                     # Send ['Guess' Input]
 
-    result_str = s.recv(1024).decode()                  # Receive ['Result' String]
+    result_str = s.recv(1024).decode()                                  # Receive ['Result' String]
     if "Correct" in result_str:
         print(result_str)
-        again_input = input('R: ').strip().upper()      # Input 'Play Again'
+        again_input = input('R: ').strip().upper()                      # Input 'Play Again'
         if again_input == 'Y': 
             diff_lock = False
-            s.sendall(again_input.encode())             # Send ['Play Again' Input]
+            s.sendall(again_input.encode())                             # Send ['Play Again' Input]
         elif again_input == 'N':
             username_lock = False
             diff_lock = False
-            s.sendall(again_input.encode())             # Send ['Play Again' Input]
+            s.sendall(again_input.encode())                             # Send ['Play Again' Input]
 
-            display_file_contents(file_path)            # Display Txt File [Line by Line, Up To 10]
+            display_file_contents(file_path)                            # Display Txt File [Line by Line, Up To 10]
 
             break
     else:
--- gg_server.py
+++ gg_server.py
@@ -23,18 +23,18 @@ correct_guess = 0
 repeat_ans = None
 
 leaderboard_dict = {} 
-diff_info = ['leaderboard_easy.txt', 100, 5]            # Default Values | file path, max score, deduction
+diff_info = ['leaderboard_easy.txt', 100, 5]                                    # Default Values | file path, max score, deduction
 
-def read_file_to_dict(file_path):                       # File to Dictionary
-    leaderboard_dict = {}                               # Fills Dictionary with Data from Txt
+def read_file_to_dict(file_path):                                               # File to Dictionary
+    leaderboard_dict = {}                                                       # Fills Dictionary with Data from Txt
     with open(file_path, 'r') as file:
         for line in file:
             name, value = line.strip().split(' : ')
             leaderboard_dict[name] = int(value)
     return leaderboard_dict
 
-def sort_and_write_to_file(data_dict, file_path):
-    sorted_items = sorted(data_dict.items(), key=lambda x: x[1], reverse=True)
+def sort_and_write_to_file(data_dict, file_path):                               # Sorts Dictionary Based on 'Scores' (Highest to Lowest)
+    sorted_items = sorted(data_dict.items(), key=lambda x: x[1], reverse=True)  # Write Mode - Writes the Sorted Items -> Txt File
     with open(file_path, 'w') as file:
         for name, value in sorted_items:
             file.write(f"{name} : {value}\n")
@@ -55,29 +55,29 @@ while True:
 
     else:
         if not username_lock:
-            client_socket.sendall(username_str.encode())                    # Send 'Username' String
-            username_input = client_socket.recv(1024)                       # Receive 'Username' Input
-            username_ans = str(username_input.decode().strip())             # Decode 'Username' Input
-            username_lock = True                                            # Lock 'Username'
+            client_socket.sendall(username_str.encode())                        # Send 'Username' String
+            username_input = client_socket.recv(1024)                           # Receive 'Username' Input
+            username_ans = str(username_input.decode().strip())                 # Decode 'Username' Input
+            username_lock = True                                                # Lock 'Username'
             print(f'Username: {username_ans}')
 
         if not diff_lock:
-            client_socket.sendall(diff_str.encode())                        # Send ['Difficulty' String]
-            diff_input = client_socket.recv(1024)                           # Receive ['Difficulty' Input]
-            diff_ans = str(diff_input.decode().strip())                     # Decode ['Difficulty' Input]
+            client_socket.sendall(diff_str.encode())                            # Send ['Difficulty' String]
+            diff_input = client_socket.recv(1024)                               # Receive ['Difficulty' Input]
+            diff_ans = str(diff_input.decode().strip())                         # Decode ['Difficulty' Input]
             print(f'Game Difficulty: {diff_ans}')
             if diff_ans == 'A': 
                 correct_guess = random.randint(1,50)
                 diff_info = ['leaderboard_easy.txt', 100, 5]
-                leaderboard_dict = read_file_to_dict(diff_info[0])          # File to Dictionary [Easy]                  
+                leaderboard_dict = read_file_to_dict(diff_info[0])              # File to Dictionary [Easy]                  
             elif diff_ans == 'B': 
                 correct_guess = random.randint(1,100)
                 diff_info = ['leaderboard_medium.txt', 200, 10]
-                leaderboard_dict = read_file_to_dict(diff_info[0])          # File to Dictionary [Medium]
+                leaderboard_dict = read_file_to_dict(diff_info[0])              # File to Dictionary [Medium]
             elif diff_ans == 'C': 
                 correct_guess = random.randint(1,500)
                 diff_info = ['leaderboard_hard.txt', 300, 15]
-                leaderboard_dict = read_file_to_dict(diff_info[0])          # File to Dictionary [Hard]
+                leaderboard_dict = read_file_to_dict(diff_info[0])              # File to Dictionary [Hard]
             print(f'Correct Guess: {correct_guess}')
 
             # Display Leaderboard [Client Side]
@@ -85,9 +85,9 @@ while True:
             diff_lock = True        
 
         guess_str = f'\n[{correct_guess}] Enter Your Guess: '
-        client_socket.sendall(guess_str.encode())                           # Send ['Guess' Input]
-        guess_input = client_socket.recv(1024)                              # Receive ['Guess' Input]
-        guess_ans = int(guess_input.decode().strip())                       # Decode ['Guess' Input]
+        client_socket.sendall(guess_str.encode())                               # Send ['Guess' Input]
+        guess_input = client_socket.recv(1024)                                  # Receive ['Guess' Input]
+        guess_ans = int(guess_input.decode().strip())                           # Decode ['Guess' Input]
         
         print(f'User Guess Attempt: {guess_ans}')
         
@@ -114,8 +114,8 @@ while True:
 
         elif guess_ans > correct_guess:
             diff_info[1] -= diff_info[2]
-            client_socket.sendall(b'Wrong Answer! (Lower)')        # Send ['Result' String]
+            client_socket.sendall(b'Wrong Answer! (Lower)')                     # Send ['Result' String]
         elif guess_ans < correct_guess:
             diff_info[1] -= diff_info[2]
-            client_socket.sendall(b'Wrong Answer! (Higher)')       # Send ['Result' String]
+            client_socket.sendall(b'Wrong Answer! (Higher)')                    # Send ['Result' String]
 s.close()
